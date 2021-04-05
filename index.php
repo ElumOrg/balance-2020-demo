00f971b68c4866ee11f316068e68ea0c17836e32
shaileshkhairnar <eluminous_developer1@eluminoustechnologies.com>
2021-04-05 12:07:48 +0530

    New design create

--- index.php
+++ index.php
@@ -1,32 +1,120 @@
 <?php 
   include('layout/header.php');
 ?>
-
-<section class="main-banner home-banner">
-</section>
-
-<section class="yoga ">
-      <div class="our_work_section">
-       
-       <div class="container mw-100">
-          <div class="row no-gutters flex-lg-row-reverse">
-             <div class="col-xl-7 col-lg-7 col-md-12 texture_banner bg_rotate banner black_11d_bg text-white d-flex align-items-center">
-                <div class="text_wrap">
-                   <h2>About Unique Wellness</h2>
-                   <p>I’ve been a recognized Yoga Instructor in San Francisco since 2023. I believe in living a life free of judgement, filled with joy and gratitude. These feelings inform and guide my teaching and practice, which is focused on helping my students better connect to themselves and their surroundings. Book a class now and start leading a healthier lifestyle..</p>               
-                </div>
-             </div>
-             <div class="col-xl-5 col-lg-5 col-md-12 d-flex align-items-center justify-content-center">
-                <div class="img_wrap">
-                   <!-- <img src="http://localhost/balance-2020/assets//frontend/images/yoga-tailored.jpg" alt="Yoga Tailored" class="img-fluid w-100"> -->
-                </div>
-             </div>
-          </div>
-       </div>
-    
+<!-- Banner Section Start -->
+<section class="home_banner banner home_banner_img">
+   <div class="container">
+      <div class="text_wrap">
+         <div class="wrap">
+            <h1>Unique Wellness</h1>
+            <p>Discover Your Path</p>
+         </div>
       </div>
+   </div>
 </section>
+<!-- Banner Section End -->
 
+<!-- At Your Service Section Start -->
+<section class="service">
+   <div class="container text-center">
+      <h2>At Your Service With You In Mind</h2>
+      <p>Here at Unique, we focus on the unique experiences and individual needs of our students.</p>
+      <p>We offer yoga, meditation, wellness, and holistic living industries, so if you are offering in-person or online classes, training sessions, coaching, workshops, events, or even retreats then Unique can help you.</p>
+      <p>Our affordable pricing and plans provide multiple high-quality wellness solutions to anyone, from anywhere, at anytime!</p>
+      <p>All Wellness Matters and More!</p>
+      <p>Mindfulness, Yoga, Fitness, Nutrition, Coaching, Counseling, Marketing & more!</p>
+      <p>At Unique, we are here to make your life as easy as possible so that you can get on with wellness</p>
+      <p>Now you can deliver the best possible experience to our clients!</p>
+   </div>
+</section>
+<!-- At Your Service Section End -->
+
+<!-- Wellness Section Start -->
+<section class="wellness bg_16e py-0">
+   <div class="container mw-100">
+      <div class="row no-gutters">
+         <div class="col-lg-6">
+            <div class="img_wrap">
+               <img src="includes/images/home-banner-2.png" alt="About Unique Wellness" class="img-fuild img" />
+            </div>
+         </div>
+         <div class="col-lg-6 d-flex justify-content-center align-items-center text-center">
+            <div class="text">
+               <h2>About Unique Wellness</h2>
+               <p>I’ve been a recognized Yoga Instructor in San Francisco since 2023. I believe in living a life free of judgement, filled with joy and gratitude. These feelings inform and guide my teaching and practice, which is focused on helping my students better connect to themselves and their surroundings. Book a class now and start leading a healthier lifestyle.</p>
+               <a href="#" class="btn white_btn">Get in Touch</a>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Wellness Section End -->
+
+<!-- Class Schedule Section Start -->
+<section class="schedule">
+   <div class="container">
+      <h2 class="text-center">Class Schedule</h2>
+   </div>
+</section>
+<!-- Class Schedule Section End -->
+
+<!-- Testimonial Section Start -->
+<section class="testi">
+   <div class="container">
+      <div class="wrap">
+         <figure>
+            <blockquote>
+               Our bodies are our gardens - our wills are our gardeners
+               <span>William Shakespeare</span>
+            </blockquote>
+         </figure>
+      </div>
+   </div>
+</section>
+<!-- Testimonial Section End -->
+
+<!-- Let’s Connect Section Start -->
+<section class="connect bg_16e">
+   <div class="container">
+      <h2 class="text-center text-white">Let’s Connect</h2>
+      <div class="row justify-content-center">
+         <div class="col-lg-8">            
+            <div class="form_wrap">
+               <form>
+                  <div class="row">
+                     <div class="col-lg-6">
+                        <div class="form-group">
+                           <input type="text" class="form-control" id="" aria-describedby="textHelp" placeholder="Name">
+                        </div>
+                     </div>
+                     <div class="col-lg-6">
+                        <div class="form-group">
+                           <input type="email" class="form-control" id="" aria-describedby="emailHelp" placeholder="Enter email">
+                        </div>
+                     </div>
+                     <div class="col-lg-12">
+                        <div class="form-group">
+                           <input type="text" class="form-control" id="" aria-describedby="textHelp" placeholder="Subject">
+                        </div>
+                     </div>
+                     <div class="col-lg-12">
+                        <div class="form-group">
+                           <textarea class="form-control" id="" placeholder="Type your message here...."></textarea>
+                        </div>
+                     </div>
+                     <div class="col-lg-12">
+                        <div class="form-group">
+                           <button type="submit" class="btn w-100">Submit</button>
+                        </div>
+                     </div>
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- Let’s Connect Section End -->
 
 <?php 
   include('layout/footer.php');
--- layout/footer.php
+++ layout/footer.php
@@ -1,5 +1,16 @@
 <footer>
-   <div class="bottom">
-      <p> © <?php echo date('Y'); ?> by Unique Wellness.</p>
+   <div class="container">
+      <p class="titile">©2021 by Unique Wellness</p>
+      <ul class="socail_menu">
+         <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+         <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+         <li><a href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+      </ul>
    </div>
 </footer>
+<script src="includes/js/jquery.min.js"></script>
+<script src="includes/js/bootstrap.min.js"></script>
+<script src="includes/js/popper.min.js"></script>
+<script src="includes/js/custom.js"></script>
+</body>
+</html>
